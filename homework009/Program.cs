d73e6a246c74d5604c7c1d486da93196a351b115
sirgak <105858790+sirgak@users.noreply.github.com>
2022-06-03 20:34:49 +0300

    test
    t

--- homework009/Program.cs
+++ homework009/Program.cs
@@ -9,15 +9,20 @@ namespace homework009
             int variable1 = 3;
             int variable2 = 5;
             int sumOfNumbers = 0;
+            int minValue = 0;
+            int maxValue = 101;
+
             Random random = new Random();
-            int number = random.Next(0, 101);
+
+            int number = random.Next(minValue, maxValue);
             Console.WriteLine("Полученное число: " + number);
+
             for (int i = 0; i < number; i++)
             {
                 if (i % variable1 == 0 || i % variable2 == 0)
                     sumOfNumbers += i;
             }
-            Console.WriteLine("Сумма всех чисел меньше " + number + " и кратные 3м и 5: " +  sumOfNumbers);
+            Console.WriteLine("Сумма всех чисел меньше " + number + " и кратные " + variable1 + " и " + variable2 +  sumOfNumbers);
         }
     }
 }
